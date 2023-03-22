a6400264144d59966001378382e61fb445b854a3
Dawid Zając <zajac.dawid.dev@gmail.com>
2023-03-22 15:29:32 +0100

    Edit report path

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://warszawskiedniinformatyki.pl',
-    responseTimeout: 10000
+    responseTimeout: 10000,
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'results/test-results.xml',
+      toConsole: true,
+    }
   },
 });
