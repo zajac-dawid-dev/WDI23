cf57b4c983e6576f52c4560133ef6ed0b6c40bd9
Dawid Zając <33731466+zajac-dawid-dev@users.noreply.github.com>
2023-03-27 00:13:00 +0200

    Bugfix: Replace preStop lifecycle with entrypoint.sh (#3)

---
+++ e2e/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm run test && curl -XPOST -u ":__USER_PAT__" -H "Content-type: application/json" "https://dev.azure.com/dejvis/wdi23/_apis/pipelines/__PIPELINE_ID__/runs?api-version=7.0" -d '{"runParameters": ""}'
